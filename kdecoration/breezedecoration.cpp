Sonic-DE/silver-window-decoration

Remove KDE/QT 5 support

kdecoration/breezedecoration.cpp
@@ -33,6 +33,7 @@
 #include <QDBusPendingCallWatcher>
 #include <QDBusPendingReply>
 #include <QPainter>
+#include <QProcess>
 #include <QTextStream>
 #include <QTimer>
 
@@ -726,7 +727,7 @@ void Decoration::setGlobalLookAndFeelOptions(QString lookAndFeelPackageName)
 
         auto presetNameIt = lfPackagePresetNames.find(lookAndFeelPackageName);
         if (presetNameIt != lfPackagePresetNames.end()) { // if matching look-and-feel-package, load the associated Silver window decoration preset
-            system("silver-settings -w \"" + presetNameIt.value().toUtf8() + "\" &");
+            QProcess::startDetached(QStringLiteral("silver-settings"), {QStringLiteral("-w"), presetNameIt.value()});
         }
     }
 }

kdecoration/config/breezeconfigwidget.cpp
@@ -77,7 +77,7 @@ ConfigWidget::ConfigWidget(QObject *parent, const KPluginMetaData &data, const Q
     widget()->setTabOrder(m_presetsButton, m_ui.tabWidget);
 
     // hide the title if not silver-settings
-    if (widget()->window() && qAppName() != "silver-settings") {
+    if (widget()->window() && qAppName() != QStringLiteral("silver-settings")) {
         m_kPageWidget = widget()->window()->findChild<KPageWidget *>();
         if (m_kPageWidget) {
             KPageWidgetItem *currentPage = m_kPageWidget->currentPage();
@@ -130,12 +130,12 @@ ConfigWidget::ConfigWidget(QObject *parent, const KPluginMetaData &data, const Q
 
 #if KLASSY_GIT_MASTER
     // set the long version string if from the git master
-    m_ui.version->setText("v" + silverLongVersion());
+    m_ui.version->setText(QStringLiteral("v") + silverLongVersion());
 
 #else
     // set shortened version string in UI if an official release
     QRegularExpression re("\\d+\\.\\d+");
-    QRegularExpressionMatch match = re.match(KLASSY_VERSION);
+    QRegularExpressionMatch match = re.match(SILVER_VERSION);
     if (match.hasMatch()) {
         QString matched = match.captured(0);
         m_ui.version->setText("v" + matched);
@@ -800,17 +800,17 @@ void ConfigWidget::getButtonsOrderFromKwinConfig()
 {
     QMap<DecorationButtonType, QChar> buttonNames;
     // list modified from https://invent.kde.org/plasma/kwin/-/blob/master/src/decorations/settings.cpp
-    buttonNames[DecorationButtonType::Menu] = QChar('M');
-    buttonNames[DecorationButtonType::ApplicationMenu] = QChar('N');
-    buttonNames[DecorationButtonType::OnAllDesktops] = QChar('S');
-    buttonNames[DecorationButtonType::KeepAbove] = QChar('F');
-    buttonNames[DecorationButtonType::KeepBelow] = QChar('B');
-    buttonNames[DecorationButtonType::Shade] = QChar('L');
-    buttonNames[DecorationButtonType::ContextHelp] = QChar('H');
-    buttonNames[DecorationButtonType::Minimize] = QChar('I');
-    buttonNames[DecorationButtonType::Maximize] = QChar('A');
-    buttonNames[DecorationButtonType::Close] = QChar('X');
-    buttonNames[DecorationButtonType::Spacer] = QChar('_');
+    buttonNames[DecorationButtonType::Menu] = QChar(u'M');
+    buttonNames[DecorationButtonType::ApplicationMenu] = QChar(u'N');
+    buttonNames[DecorationButtonType::OnAllDesktops] = QChar(u'S');
+    buttonNames[DecorationButtonType::KeepAbove] = QChar(u'F');
+    buttonNames[DecorationButtonType::KeepBelow] = QChar(u'B');
+    buttonNames[DecorationButtonType::Shade] = QChar(u'L');
+    buttonNames[DecorationButtonType::ContextHelp] = QChar(u'H');
+    buttonNames[DecorationButtonType::Minimize] = QChar(u'I');
+    buttonNames[DecorationButtonType::Maximize] = QChar(u'A');
+    buttonNames[DecorationButtonType::Close] = QChar(u'X');
+    buttonNames[DecorationButtonType::Spacer] = QChar(u'_');
 
     QString buttonsOnLeft;
     QString buttonsOnRight;
@@ -880,22 +880,22 @@ void ConfigWidget::getButtonsOrderFromKwinConfig()
 
     // find leftEdgePriorityButtons
     for (int i = 0; i < 3; i++) {
-        if (buttonsOnLeft.indexOf(QChar('X')) == i) {
+        if (buttonsOnLeft.indexOf(QChar(u'X')) == i) {
             leftEdgePriorityButtons.insert(i, DecorationButtonType::Close);
-        } else if (buttonsOnLeft.indexOf(QChar('A')) == i) {
+        } else if (buttonsOnLeft.indexOf(QChar(u'A')) == i) {
             leftEdgePriorityButtons.insert(i, DecorationButtonType::Maximize);
-        } else if (buttonsOnLeft.indexOf(QChar('I')) == i) {
+        } else if (buttonsOnLeft.indexOf(QChar(u'I')) == i) {
             leftEdgePriorityButtons.insert(i, DecorationButtonType::Minimize);
         }
     }
 
     // find rightEdgePrioritybuttons
     for (int i = m_visibleButtonsOrder.count() - 1; i >= m_visibleButtonsOrder.count() - 3; i--) {
-        if (buttonsOnRight.lastIndexOf(QChar('X')) == i) { // lastIndexOf in-case a weirdo adds more than one button of the same type
+        if (buttonsOnRight.lastIndexOf(QChar(u'X')) == i) { // lastIndexOf in-case a weirdo adds more than one button of the same type
             rightEdgePriorityButtons.insert(i, DecorationButtonType::Close);
-        } else if (buttonsOnRight.lastIndexOf(QChar('A')) == i) {
+        } else if (buttonsOnRight.lastIndexOf(QChar(u'A')) == i) {
             rightEdgePriorityButtons.insert(i, DecorationButtonType::Maximize);
-        } else if (buttonsOnRight.lastIndexOf(QChar('I')) == i) {
+        } else if (buttonsOnRight.lastIndexOf(QChar(u'I')) == i) {
             rightEdgePriorityButtons.insert(i, DecorationButtonType::Minimize);
         }
     }

kstyle/config/breezestyleconfig.cpp
@@ -15,7 +15,9 @@
 #endif
 
 #include <QDialog>
+#include <QObject>
 #include <QRegularExpression>
+#include <QWidget>
 
 extern "C" {
 Q_DECL_EXPORT QWidget *allocate_kstyle_config(QWidget *parent)
@@ -54,12 +56,12 @@ StyleConfig::StyleConfig(QWidget *parent)
 
 #if KLASSY_GIT_MASTER
     // set the long version string if from the git master
-    _version->setText("v" + QString(KLASSY_VERSION) + ".git");
+    _version->setText(QStringLiteral("v") + QStringLiteral(SILVER_VERSION) + QStringLiteral(".git"));
 
 #else
     // set shortened version string in UI if an official release
     QRegularExpression re("\\d+\\.\\d+");
-    QRegularExpressionMatch match = re.match(KLASSY_VERSION);
+    QRegularExpressionMatch match = re.match(SILVER_VERSION);
     if (match.hasMatch()) {
         QString matched = match.captured(0);
         _version->setText("v" + matched);
@@ -157,7 +159,7 @@ void StyleConfig::defaults()
     StyleConfigData::self()->setDefaults();
     load();
 
-    emit changed(!isDefaults());
+    Q_EMIT changed(!isDefaults());
 }
 
 bool StyleConfig::isDefaults()
@@ -246,7 +248,7 @@ void StyleConfig::updateChanged()
     else if (_menuOpacity->value() != StyleConfigData::menuOpacity())
         modified = true;
 
-    emit changed(modified);
+    Q_EMIT changed(modified);
 }
 
 //__________________________________________________________________

kstyle/config/main.cpp
@@ -72,32 +72,28 @@ CommandLineProcessResult processComandLine(QApplication &app, QCommandLineParser
     parser.addHelpOption();
     parser.addVersionOption();
 
-    QCommandLineOption importOption(QStringList() << "i"
-                                                  << "import-preset",
+    QCommandLineOption importOption(QStringList() << QStringLiteral("i") << QStringLiteral("import-preset"),
                                     i18n("Import a Silver .klpw Preset File with filename <preset filename>."),
                                     i18n("preset filename"));
     parser.addOption(importOption);
 
-    QCommandLineOption loadWindecoPresetOption(QStringList() << "w"
-                                                             << "load-windeco-preset",
+    QCommandLineOption loadWindecoPresetOption(QStringList() << QStringLiteral("w") << QStringLiteral("load-windeco-preset"),
                                                i18n("Load the Window Decoration Preset, with name <preset name>."),
                                                i18n("preset name"));
     parser.addOption(loadWindecoPresetOption);
 
-    QCommandLineOption forceOption(QStringList() << "f"
-                                                 << "force-import-invalid-version",
+    QCommandLineOption forceOption(QStringList() << QStringLiteral("f") << QStringLiteral("force-import-invalid-version"),
                                    i18n("Force the import of a preset file from a different Silver version."));
     parser.addOption(forceOption);
 
-    QCommandLineOption generateIcons(QStringList() << "g"
-                                                   << "generate-system-icons",
+    QCommandLineOption generateIcons(QStringList() << QStringLiteral("g") << QStringLiteral("generate-system-icons"),
                                      i18n("Generate silver and silver-dark system icons."));
     parser.addOption(generateIcons);
 
     parser.process(app);
 
-    char const *configFile = "silver/silverrc";
-    char const *presetsConfigFile = "silver/windecopresetsrc";
+    QString const configFile = QStringLiteral("silver/silverrc");
+    QString const presetsConfigFile = QStringLiteral("silver/windecopresetsrc");
     QTextStream output(stdout);
     bool commandSet = false;
     if (parser.isSet(importOption)) {

libbreezecommon/breeze.cpp
@@ -13,9 +13,9 @@ QString silverLongVersion()
 {
     QString version;
 
-    version = QString(KLASSY_VERSION);
+    version = QStringLiteral(SILVER_VERSION);
 #if KLASSY_GIT_MASTER
-    version += ".git";
+    version += QStringLiteral(".git");
 #endif
     return version;
 }

Sonic-DE/silver-theme

(cherry picked from commit d3d2af06813f2eb374a1a506a578e4c3245b45b8)

kdecoration/breezedecoration.cpp
@@ -35,6 +35,7 @@
 #include <QDBusPendingReply>
 #include <QHoverEvent>
 #include <QPainter>
+#include <QProcess>
 #include <QTextStream>
 #include <QTimer>
 
@@ -298,7 +299,7 @@ void Decoration::init()
     auto c = window();
 
     reconfigureMain(true);
-    
+
     // active state change animation
     // It is important start and end value are of the same type, hence 0.0 and not just 0
     m_animation->setStartValue(0.0);
@@ -827,7 +828,7 @@ void Decoration::setGlobalLookAndFeelOptions(QString lookAndFeelPackageName)
         }
 
         if (!presetToLoad.isEmpty()) { // if matching look-and-feel-package, load the associated Klassy window decoration preset
-            system("klassy-settings -w \"" + presetToLoad.toUtf8() + "\" &");
+            QProcess::startDetached(QStringLiteral("klassy-settings"), {QStringLiteral("-w"), presetToLoad.toUtf8()});
         }
     }
 }

kdecoration/config/breezeconfigwidget.cpp
@@ -81,7 +81,7 @@ ConfigWidget::ConfigWidget(QObject *parent, const KPluginMetaData &data, const Q
     widget()->setTabOrder(m_presetsButton, m_ui.tabWidget);
 
     // hide the title if not klassy-settings
-    if (widget()->window() && qAppName() != "klassy-settings") {
+    if (widget()->window() && qAppName() != QStringLiteral("klassy-settings")) {
         m_kPageWidget = widget()->window()->findChild<KPageWidget *>();
         if (m_kPageWidget) {
             KPageWidgetItem *currentPage = m_kPageWidget->currentPage();
@@ -136,7 +136,7 @@ ConfigWidget::ConfigWidget(QObject *parent, const KPluginMetaData &data, const Q
     connect(this, &ConfigWidget::saved, m_buttonColorsDialog, &ButtonColors::load);
 
     // set the long version string if from the git master
-    m_ui.version->setText("v" + klassyLongVersion());
+    m_ui.version->setText(QStringLiteral("v") + klassyLongVersion());
 
     connect(m_ui.systemIconGenerationButton, &QAbstractButton::clicked, this, &ConfigWidget::systemIconGenerationButtonClicked);
     connect(m_ui.buttonSizingButton, &QAbstractButton::clicked, this, &ConfigWidget::buttonSizingButtonClicked);
@@ -828,17 +828,17 @@ void ConfigWidget::getButtonsOrderFromKwinConfig()
 {
     QMap<DecorationButtonType, QChar> buttonNames;
     // list modified from https://invent.kde.org/plasma/kwin/-/blob/master/src/decorations/settings.cpp
-    buttonNames[DecorationButtonType::Menu] = QChar('M');
-    buttonNames[DecorationButtonType::ApplicationMenu] = QChar('N');
-    buttonNames[DecorationButtonType::OnAllDesktops] = QChar('S');
-    buttonNames[DecorationButtonType::KeepAbove] = QChar('F');
-    buttonNames[DecorationButtonType::KeepBelow] = QChar('B');
-    buttonNames[DecorationButtonType::Shade] = QChar('L');
-    buttonNames[DecorationButtonType::ContextHelp] = QChar('H');
-    buttonNames[DecorationButtonType::Minimize] = QChar('I');
-    buttonNames[DecorationButtonType::Maximize] = QChar('A');
-    buttonNames[DecorationButtonType::Close] = QChar('X');
-    buttonNames[DecorationButtonType::Spacer] = QChar('_');
+    buttonNames[DecorationButtonType::Menu] = QChar(u'M');
+    buttonNames[DecorationButtonType::ApplicationMenu] = QChar(u'N');
+    buttonNames[DecorationButtonType::OnAllDesktops] = QChar(u'S');
+    buttonNames[DecorationButtonType::KeepAbove] = QChar(u'F');
+    buttonNames[DecorationButtonType::KeepBelow] = QChar(u'B');
+    buttonNames[DecorationButtonType::Shade] = QChar(u'L');
+    buttonNames[DecorationButtonType::ContextHelp] = QChar(u'H');
+    buttonNames[DecorationButtonType::Minimize] = QChar(u'I');
+    buttonNames[DecorationButtonType::Maximize] = QChar(u'A');
+    buttonNames[DecorationButtonType::Close] = QChar(u'X');
+    buttonNames[DecorationButtonType::Spacer] = QChar(u'_');
 
     QString buttonsOnLeft;
     QString buttonsOnRight;
@@ -908,22 +908,22 @@ void ConfigWidget::getButtonsOrderFromKwinConfig()
 
     // find leftEdgePriorityButtons
     for (int i = 0; i < 3; i++) {
-        if (buttonsOnLeft.indexOf(QChar('X')) == i) {
+        if (buttonsOnLeft.indexOf(QChar(u'X')) == i) {
             leftEdgePriorityButtons.insert(i, DecorationButtonType::Close);
-        } else if (buttonsOnLeft.indexOf(QChar('A')) == i) {
+        } else if (buttonsOnLeft.indexOf(QChar(u'A')) == i) {
             leftEdgePriorityButtons.insert(i, DecorationButtonType::Maximize);
-        } else if (buttonsOnLeft.indexOf(QChar('I')) == i) {
+        } else if (buttonsOnLeft.indexOf(QChar(u'I')) == i) {
             leftEdgePriorityButtons.insert(i, DecorationButtonType::Minimize);
         }
     }
 
     // find rightEdgePrioritybuttons
     for (int i = m_visibleButtonsOrder.count() - 1; i >= m_visibleButtonsOrder.count() - 3; i--) {
-        if (buttonsOnRight.lastIndexOf(QChar('X')) == i) { // lastIndexOf in-case a weirdo adds more than one button of the same type
+        if (buttonsOnRight.lastIndexOf(QChar(u'X')) == i) { // lastIndexOf in-case a weirdo adds more than one button of the same type
             rightEdgePriorityButtons.insert(i, DecorationButtonType::Close);
-        } else if (buttonsOnRight.lastIndexOf(QChar('A')) == i) {
+        } else if (buttonsOnRight.lastIndexOf(QChar(u'A')) == i) {
             rightEdgePriorityButtons.insert(i, DecorationButtonType::Maximize);
-        } else if (buttonsOnRight.lastIndexOf(QChar('I')) == i) {
+        } else if (buttonsOnRight.lastIndexOf(QChar(u'I')) == i) {
             rightEdgePriorityButtons.insert(i, DecorationButtonType::Minimize);
         }
     }

kstyle/config/breezestyleconfig.cpp
@@ -15,7 +15,9 @@
 #endif
 
 #include <QDialog>
+#include <QObject>
 #include <QRegularExpression>
+#include <QWidget>
 
 extern "C" {
 Q_DECL_EXPORT QWidget *allocate_kstyle_config(QWidget *parent)
@@ -54,10 +56,10 @@ StyleConfig::StyleConfig(QWidget *parent)
 
 #if KLASSY_GIT_MASTER
     // set the long version string if from the git master
-    _version->setText("v" + QString(KLASSY_VERSION) + ".git");
+    _version->setText(QStringLiteral("v") + QStringLiteral(KLASSY_VERSION) + QStringLiteral(".git"));
 
 #else
-    _version->setText("v" + QString(KLASSY_VERSION));
+    _version->setText(QStringLiteral("v") + QStringLiteral(KLASSY_VERSION));
 #endif
 
     // add corner icon
@@ -151,7 +153,7 @@ void StyleConfig::defaults()
     StyleConfigData::self()->setDefaults();
     load();
 
-    emit changed(!isDefaults());
+    Q_EMIT changed(!isDefaults());
 }
 
 bool StyleConfig::isDefaults()
@@ -240,7 +242,7 @@ void StyleConfig::updateChanged()
     else if (_menuOpacity->value() != StyleConfigData::menuOpacity())
         modified = true;
 
-    emit changed(modified);
+    Q_EMIT changed(modified);
 }
 
 //__________________________________________________________________

kstyle/config/main.cpp
@@ -72,32 +72,28 @@ CommandLineProcessResult processComandLine(QApplication &app, QCommandLineParser
     parser.addHelpOption();
     parser.addVersionOption();
 
-    QCommandLineOption importOption(QStringList() << "i"
-                                                  << "import-preset",
+    QCommandLineOption importOption(QStringList() << QStringLiteral("i") << QStringLiteral("import-preset"),
                                     i18n("Import a Klassy .klpw Preset File with filename <preset filename>."),
                                     i18n("preset filename"));
     parser.addOption(importOption);
 
-    QCommandLineOption loadWindecoPresetOption(QStringList() << "w"
-                                                             << "load-windeco-preset",
+    QCommandLineOption loadWindecoPresetOption(QStringList() << QStringLiteral("w") << QStringLiteral("load-windeco-preset"),
                                                i18n("Load the Window Decoration Preset, with name <preset name>."),
                                                i18n("preset name"));
     parser.addOption(loadWindecoPresetOption);
 
-    QCommandLineOption forceOption(QStringList() << "f"
-                                                 << "force-import-invalid-version",
+    QCommandLineOption forceOption(QStringList() << QStringLiteral("f") << QStringLiteral("force-import-invalid-version"),
                                    i18n("Force the import of a preset file from a different Klassy version."));
     parser.addOption(forceOption);
 
-    QCommandLineOption generateIcons(QStringList() << "g"
-                                                   << "generate-system-icons",
+    QCommandLineOption generateIcons(QStringList() << QStringLiteral("g") << QStringLiteral("generate-system-icons"),
                                      i18n("Generate klassy and klassy-dark system icons."));
     parser.addOption(generateIcons);
 
     parser.process(app);
 
-    char const *configFile = "klassy/klassyrc";
-    char const *presetsConfigFile = "klassy/windecopresetsrc";
+    QString const configFile = QStringLiteral("klassy/klassyrc");
+    QString const presetsConfigFile = QStringLiteral("klassy/windecopresetsrc");
     QTextStream output(stdout);
     bool commandSet = false;
     if (parser.isSet(importOption)) {

libbreezecommon/breeze.cpp
@@ -13,9 +13,9 @@ QString klassyLongVersion()
 {
     QString version;
 
-    version = QString(KLASSY_VERSION);
+    version = QStringLiteral(KLASSY_VERSION);
 #if KLASSY_GIT_MASTER
-    version += ".git";
+    version += QStringLiteral(".git");
 #endif
     return version;
 }